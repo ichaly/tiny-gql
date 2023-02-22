b88e160298c437943f7e3675a4b86875f1c30932
iChaly <i@eunut.com>
2023-02-22 16:36:16 +0800

    refactor: change schema field type

--- core/schema.go
+++ core/schema.go
@@ -2,6 +2,8 @@ package core
 
 import (
 	"encoding/json"
+	"fmt"
+	"github.com/iancoleman/strcase"
 )
 
 //
@@ -9,18 +11,36 @@ import (
 // https://spec.graphql.org/draft/#sec-Schema-Introspection
 //
 
-type __Schema struct {
-	Types            []__Type      `json:"types,omitempty"`
-	Directives       []__Directive `json:"directives,omitempty"`
-	Description      string        `json:"description,omitempty"`
-	QueryType        __Type        `json:"queryType,omitempty"`
-	MutationType     *__Type       `json:"mutationType,omitempty"`
-	SubscriptionType *__Type       `json:"subscriptionType,omitempty"`
+type mapType map[string]__Type
 
-	conf       Config
-	info       *DBInfo
-	types      map[string]bool
-	directives map[string]bool
+func (my mapType) MarshalJSON() ([]byte, error) {
+	var list []__Type
+	for _, t := range my {
+		list = append(list, t)
+	}
+	return json.Marshal(list)
+}
+
+type mapDirective map[string]__Directive
+
+func (my mapDirective) MarshalJSON() ([]byte, error) {
+	var list []__Directive
+	for _, t := range my {
+		list = append(list, t)
+	}
+	return json.Marshal(list)
+}
+
+type __Schema struct {
+	Types            mapType      `json:"types,omitempty"`
+	Directives       mapDirective `json:"directives,omitempty"`
+	Description      string       `json:"description,omitempty"`
+	QueryType        __Type       `json:"queryType,omitempty"`
+	MutationType     *__Type      `json:"mutationType,omitempty"`
+	SubscriptionType *__Type      `json:"subscriptionType,omitempty"`
+
+	conf *Config
+	info *DBInfo
 }
 
 type __Type struct {
@@ -99,22 +119,22 @@ const (
 	DL_INPUT_FIELD_DEFINITION __DirectiveLocation = "INPUT_FIELD_DEFINITION"
 )
 
-func (my *__Schema) addType(t __Type) {
-	if _, ok := my.types[t.Name]; ok {
-		return
+func (my *__Schema) getName(name string) string {
+	if my.conf.EnableCamelcase {
+		return strcase.ToCamel(name)
+	} else {
+		return name
 	}
-	my.Types = append(my.Types, t)
+}
+
+func (my *__Schema) addType(t __Type) {
+	my.Types[t.Name] = t
 }
 
 func (my *__Schema) addExpression(exps []__Field, name string, sub __Type) {
-	name = name + SUFFIX_EXP
-	if _, ok := my.types[name]; ok {
-		return
-	}
-
 	t := __Type{
 		Kind:        TK_INPUT_OBJECT,
-		Name:        name,
+		Name:        name + SUFFIX_EXP,
 		InputFields: []__Field{},
 	}
 
@@ -127,8 +147,197 @@ func (my *__Schema) addExpression(exps []__Field, name string, sub __Type) {
 	my.addType(t)
 }
 
-func Introspection() (res json.RawMessage, err error) {
+func (my *__Schema) addTablesEnumType() {
+	te := __Type{
+		Kind:        KIND_ENUM,
+		Name:        "Tables" + SUFFIX_ENUM,
+		Description: "All available tables",
+	}
+
+	for _, t := range my.info.Tables {
+		if t.Blocked {
+			continue
+		}
+		f := __Field{Name: my.getName(t.Name)}
+		if t.Comment != nil {
+			f.Description = *t.Comment
+		}
+		te.EnumValues = append(te.EnumValues, f)
+	}
+
+	my.addType(te)
+}
+
+//func (my *__Schema) addTypeTo(op string, ft __Type) {
+//	qt := my.types[op]
+//	qt.Fields = append(qt.Fields, fieldObj{
+//		Name:        ft.Name,
+//		Description: ft.Description,
+//		Args:        ft.InputFields,
+//		Type:        newTR("", ft.Name, nil),
+//	})
+//	my.types[op] = qt
+//}
+//
+//func (my *__Schema) addTable(t *DBTable, alias string) (err error) {
+//	if t.Blocked || len(t.Columns) == 0 {
+//		return
+//	}
+//	var tq __Type
+//
+//	// add table type to query and subscription
+//	if tq, err = my.addTableType(t, alias, 0); err != nil {
+//		return
+//	}
+//	my.addTypeTo("Query", tq)
+//	my.addTypeTo("Subscription", tq)
+//
+//	var tm __Type
+//
+//	// add table type to mutation
+//	if tm, err = my.addInputType(t, tq); err != nil {
+//		return
+//	}
+//	my.addTypeTo("Mutation", tm)
+//	return
+//}
+//
+//func (my *__Schema) addTableType(t *DBTable, alias string, depth int) (ft __Type, err error) {
+//	ft = __Type{
+//		Kind:        KIND_OBJECT,
+//		InputFields: []__Field{},
+//		Interfaces:  []__Type{},
+//	}
+//
+//	name := t.Name
+//	if alias != "" {
+//		name = alias
+//	}
+//	name = my.getName(name)
+//
+//	ft.Name = name
+//	if t.Comment != nil {
+//		ft.Description = *t.Comment
+//	}
+//
+//	var hasSearch bool
+//	var hasRecursive bool
+//
+//	if err = my.addColumnsEnumType(t); err != nil {
+//		return
+//	}
+//
+//	for _, fn := range my.schema.GetFunctions() {
+//		ty := my.addArgsType(t, fn)
+//		my.addType(ty)
+//	}
+//
+//	for _, c := range t.Columns {
+//		if c.Blocked {
+//			continue
+//		}
+//		if c.FullText {
+//			hasSearch = true
+//		}
+//		if c.FKRecursive {
+//			hasRecursive = true
+//		}
+//		var f1 fieldObj
+//		f1, err = my.getColumnField(c)
+//		if err != nil {
+//			return
+//		}
+//		ft.Fields = append(ft.Fields, f1)
+//	}
+//
+//	for _, fn := range my.schema.GetFunctions() {
+//		f1 := my.getFunctionField(t, fn)
+//		ft.Fields = append(ft.Fields, f1)
+//	}
+//
+//	relNodes1, err := my.schema.GetFirstDegree(t)
+//	if err != nil {
+//		return
+//	}
+//
+//	relNodes2, err := my.schema.GetSecondDegree(t)
+//	if err != nil {
+//		return
+//	}
+//
+//	for _, relNode := range append(relNodes1, relNodes2...) {
+//		var f fieldObj
+//		var skip bool
+//		f, skip, err = my.getTableField(relNode)
+//		if err != nil {
+//			return
+//		}
+//		if !skip {
+//			ft.Fields = append(ft.Fields, f)
+//		}
+//	}
+//
+//	ft.addArg("id", newTR("", "ID", nil))
+//	ft.addArg("limit", newTR("", "Int", nil))
+//	ft.addArg("offset", newTR("", "Int", nil))
+//	ft.addArg("distinctOn", newTR("LIST", "", newTR("", "String", nil)))
+//	ft.addArg("first", newTR("", "Int", nil))
+//	ft.addArg("last", newTR("", "Int", nil))
+//	ft.addArg("after", newTR("", "Cursor", nil))
+//	ft.addArg("before", newTR("", "Cursor", nil))
+//
+//	my.addOrderByType(t, &ft)
+//	my.addWhereType(t, &ft)
+//	my.addTableArgsType(t, &ft)
+//
+//	if hasSearch {
+//		ft.addArg("search", newTR("", "String", nil))
+//	}
+//
+//	if depth > 1 {
+//		return
+//	}
+//	if depth > 0 {
+//		ft.addArg("find", newTR("", "FindSearchInput", nil))
+//	}
+//
+//	my.addType(ft)
+//
+//	if hasRecursive {
+//		_, err = my.addTableTypeWithDepth(t,
+//			(name + "Recursive"),
+//			(depth + 1))
+//	}
+//	return
+//}
+
+func (my *__Schema) addColumnsEnumType(t *DBTable) (err error) {
+	tableName := my.getName(t.Name)
+	ft := __Type{
+		Kind:        KIND_ENUM,
+		Name:        tableName + "Columns" + SUFFIX_ENUM,
+		Description: fmt.Sprintf("Table columns for '%s'", tableName),
+	}
+	for _, c := range t.Columns {
+		if c.Blocked {
+			continue
+		}
+		f := __Field{Name: my.getName(c.Name)}
+		if t.Comment != nil {
+			f.Description = *c.Comment
+		}
+		ft.EnumValues = append(ft.EnumValues, f)
+	}
+	my.addType(ft)
+	return
+}
+
+func NewSchema(conf *Config, info *DBInfo) (res json.RawMessage, err error) {
 	schema := __Schema{
+		conf:             conf,
+		info:             info,
+		Types:            map[string]__Type{},
+		Directives:       map[string]__Directive{},
 		QueryType:        __Type{Name: "Query"},
 		MutationType:     &__Type{Name: "Mutation"},
 		SubscriptionType: &__Type{Name: "Subscription"},
@@ -156,6 +365,14 @@ func Introspection() (res json.RawMessage, err error) {
 	// JsonExpression types
 	v = append(expAll, expJSON...)
 	schema.addExpression(v, "JSON", __Type{Kind: TK_SCALAR, Name: "String"})
+
+	schema.addTablesEnumType()
+
+	//for _, t := range schema.info.Tables {
+	//	if err = schema.addTable(t, ""); err != nil {
+	//		return
+	//	}
+	//}
 
 	return json.Marshal(schema)
 }
--- test/schema_test.go
+++ test/schema_test.go
@@ -1,12 +1,33 @@
 package test
 
 import (
+	"database/sql"
 	"github.com/ichaly/tiny-go/core"
+	"path/filepath"
 	"testing"
 )
 
-func TestIntrospection(t *testing.T) {
-	in, err := core.Introspection()
+func init() {
+	var err error
+	dialect = "postgres"
+	url := "postgres://postgres:postgres@localhost:5432/test_development?sslmode=disable"
+	db, err = sql.Open("pgx", url)
+	if err != nil {
+		panic(err)
+	}
+}
+func TestNewSchema(t *testing.T) {
+	conf, err := core.ReadInConfig(filepath.Join("./cfg", "prod.yml"))
+	if err != nil {
+		panic(err)
+		return
+	}
+	info, err := core.GetDBInfo(db, dialect, conf.Blocklist)
+	if err != nil {
+		panic(err)
+		return
+	}
+	in, err := core.NewSchema(conf, info)
 	if err == nil {
 		println(string(in))
 	} else {
